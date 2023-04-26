d4ebcf786f5bcef364f4572c85cc0bb046759d5b
igo320345 <igo320341@gmail.com>
2023-04-26 12:36:22 +0300

    fetching data

--- lab10.py
+++ lab10.py
@@ -2,15 +2,13 @@ from vosk import Model, KaldiRecognizer
 import json
 import wave
 import pyttsx3
+import requests
 
 model = Model('vosk-model-small-ru-0.4')
-
 wf = wave.open('demo.wav', "rb")
 rec = KaldiRecognizer(model, 32000)
-
 result = ''
 last_n = False
-
 while True:
     data = wf.readframes(32000)
     if len(data) == 0:
@@ -28,7 +26,6 @@ while True:
 
 res = json.loads(rec.FinalResult())
 result += f" {res['text']}"
-
 print(result)
 
 #Works only on MacOS
@@ -36,3 +33,16 @@ engine = pyttsx3.init('nsss')
 engine.setProperty('voice', 'com.apple.speech.synthesis.voice.yuri')
 engine.say(result)
 engine.runAndWait()
+
+class Event():
+    def __init__(self, localName, date):
+        self.localName = localName
+        self.date = date
+
+
+responce = requests.get('https://date.nager.at/api/v2/publicholidays/2023/RU').json()
+events = []
+for value in  responce:
+    event = Event(value['localName'], value['date'])
+    events.append(event)
+    print(event.date, event.localName)
